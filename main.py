8f828233bd687de384209cf77fafb43cdd87a289
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-02-21 15:14:24 +0100

    Added data loader and training template

---
+++ main.py
@@ -0,0 +1,116 @@
+import sys
+import os
+import argparse
+import time
+import torch
+import numpy as np
+
+# Apex for mixed-precision training
+from apex import amp
+
+# Sacred
+from sacred import Experiment
+from sacred.stflow import LogFileWriter
+from sacred.observers import FileStorageObserver, MongoObserver
+
+# TensorBoard
+from torch.utils.tensorboard import SummaryWriter
+
+
+from utils.yaml_config_hook import yaml_config_hook
+from data.loaders import librispeech_loader
+
+#### pass configuration
+ex = Experiment("contrastive-predictive-coding")
+
+#### file output directory
+ex.observers.append(FileStorageObserver("./logs"))
+
+#### database output
+ex.observers.append(
+    MongoObserver().create(
+        url=f"mongodb://admin:admin@localhost:27017/?authMechanism=SCRAM-SHA-1",
+        db_name="db",
+    )
+)
+
+
+@ex.config
+def my_config():
+    yaml_config_hook("./config/audio/config.yaml", ex)
+
+    # override any settings here
+    # start_epoch = 100
+    # ex.add_config(
+    #   {'start_epoch': start_epoch})
+
+
+def train(args):
+
+    # get datasets and dataloaders
+    (train_loader, train_dataset, test_loader, test_dataset,) = librispeech_loader(
+        args, num_workers=args.num_workers
+    )
+
+    total_step = len(train_loader)
+    print_idx = 1
+
+    start_time = time.time()
+    for epoch in range(args.start_epoch, args.start_epoch + args.num_epochs):
+
+        loss_epoch = [0 for i in range(args.model_splits)]
+        for step, (audio, filename, _, start_idx) in enumerate(train_loader):
+
+            if step % print_idx == 0:
+                print(
+                    "Epoch [{}/{}], Step [{}/{}], Time (s): {:.3f}".format(
+                        epoch + 1,
+                        args.start_epoch + args.num_epochs,
+                        step,
+                        total_step,
+                        time.time() - start_time,
+                    )
+                )
+
+            start_time = time.time()
+
+
+@ex.automain
+@LogFileWriter(ex)
+def main(_run, _log):
+    args = argparse.Namespace(**_run.config)
+
+    if len(_run.observers) > 1:
+        out_dir = _run.observers[1].dir
+    else:
+        out_dir = _run.observers[0].dir
+
+    args.out_dir = out_dir
+
+    # set start time
+    args.time = time.ctime()
+
+    # Device configuration
+    args.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # set random seeds
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    # initialize logger
+
+    # load model
+
+    # set comment to experiment's name
+    tb_dir = os.path.join(out_dir, _run.experiment_info["name"])
+    os.makedirs(tb_dir)
+    writer = SummaryWriter(log_dir=tb_dir)
+
+    try:
+        train(args)  # , model, optimizer, writer, logs)
+    except KeyboardInterrupt:
+        print("Interrupting training, saving logs")
+
+
+if __name__ == "__main__":
+    main()
