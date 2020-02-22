4306fa9cbe09acea80da7d5c22a17fed9f83933f
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-02-22 13:51:26 +0100

    Added train script

---
+++ train_audio.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Using Sacred to log experiments\n"
+python main.py --name cpc_audio \
+    with \
+    num_epochs=300 \
+    start_epoch=0 \
+    fp16=True \
+    fp16_opt_level="O2"
